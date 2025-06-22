6c4fd2368956ee0e3b8795264c187542fdca08eb
Alisher Satbayev <alishersatbaev96@gmail.com>
2025-06-22 16:28:46 +0500

    Initial commit

---
+++ Backend/api/urls.py
@@ -0,0 +1,16 @@
+from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from api.views import PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
